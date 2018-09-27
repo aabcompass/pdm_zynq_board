75db543322235cd92cf35dc7a1c6dc0598e5076e
Alexander Belov <aabcad@gmail.com>
2018-09-27 20:49:38 +0300

    parsing is now OK

--- project_1.sdk/lwip_proj/src/gps_parser.c
+++ project_1.sdk/lwip_proj/src/gps_parser.c
@@ -8,6 +8,13 @@
 #include "gps_parser.h"
 #include "xbasic_types.h"
 #include "xparameters.h"
+#include "xil_printf.h"
+#include "stdio.h"
+
+char msg_str[NMEA0183_MAX_STR_LEN];
+int msg_pos=0;
+
+GPS_struct gps_struct;
 
 //returns 1 if NMEA0183 char is present
 int GetNextChar(char* data)
@@ -24,10 +31,50 @@ int GetNextChar(char* data)
 	}
 }
 
+void ProcessNMEA0183msg()
+{
+	int ret;
+	//example
+	//$GPGGA,162100.00,5541.91417,N,03732.45924,E,1,07,1.14,202.0,M,13.4,M,,*59
+	ret = sscanf(msg_str+1,
+			"GPGGA,%f,%f,%c,%f,%c,%d,%d,%f,%f,M,%f,M",
+			&gps_struct.time_UTC,
+			&gps_struct.latitude,
+			&gps_struct.N_or_S,
+			&gps_struct.longitude,
+			&gps_struct.E_or_W,
+			&gps_struct.fix_quality,
+			&gps_struct.satellites_num,
+			&gps_struct.horiz_dilution,
+			&gps_struct.altitude,
+			&gps_struct.geoid_height);
+	//xil_printf("ret=%d\n\r", ret);
+	//xil_printf("msg=%s\n\r", msg_str+1);
+	if(ret >= 1)
+	{
+		gps_struct.n_params = ret;
+		printf("UTC=%f,%d satellites\n\r",
+			gps_struct.time_UTC,
+			gps_struct.satellites_num);
+	}
+}
+
 void GPS_service()
 {
 	char c; int char_exists;
 	char_exists = GetNextChar(&c);
 	if(char_exists)
-		xil_printf("%c", c);
+	{
+		if(c == '$')
+		{
+			msg_pos = 0;
+			ProcessNMEA0183msg();
+		}
+		if(msg_pos >= NMEA0183_MAX_STR_LEN) return;
+		msg_str[msg_pos] = c;
+		msg_pos++;
+		//xil_printf("%c", c);
+	}
 }
+
+
--- project_1.sdk/lwip_proj/src/gps_parser.h
+++ project_1.sdk/lwip_proj/src/gps_parser.h
@@ -8,6 +8,8 @@
 #ifndef SRC_GPS_PARSER_H_
 #define SRC_GPS_PARSER_H_
 
+#define NMEA0183_MAX_STR_LEN	100
+
 #define REGW_GPS_CTRL		0
 #define REGR_GPS_NMEA0183	1
 #define REGR_GPS_FIFO_OCC	2
@@ -23,6 +25,19 @@
 
 int GetNextChar(char* data);
 
-
+typedef struct
+{
+	float time_UTC;
+	float latitude;
+	char N_or_S;
+	float longitude;
+	char E_or_W;
+	int fix_quality;
+	int satellites_num;
+	float horiz_dilution;
+	float altitude;
+	float geoid_height;
+	int n_params; // number of params presented in NMEA message
+} GPS_struct;
 
 #endif /* SRC_GPS_PARSER_H_ */
