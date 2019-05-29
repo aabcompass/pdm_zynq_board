51037ac76694e4641c1317aa49a8a83fb325bb63
Alexander Belov <aabcad@gmail.com>
2019-05-29 19:35:47 +0200

    hv_cathode.h separated to another file. data_provider tigs.xdc introduced. PL version constant introlduced (connected to AXI_GPIO0). instrument ver updated. data_provider.vhd (added ec_signals forming). axi_cathode_ctrl.vhd added

---
+++ ip_repo/data_provider/data_provider.srcs/constrs_1/new/tigs.xdc
@@ -0,0 +1,1 @@
+set_false_path -through [list [get_nets -hierarchical *adcv_max_asic_cnts*] [get_nets -hierarchical *adcv_max_pixel_num*]]
---
+++ project_1.sdk/lwip_proj/src/hv_cathode.h
@@ -0,0 +1,20 @@
+/*
+ * hv_cathode.h
+ *
+ *  Created on: May 29, 2019
+ *      Author: alx
+ */
+
+#ifndef SRC_HV_CATHODE_H_
+#define SRC_HV_CATHODE_H_
+
+// HV CATHODE regs
+#define REGW_HVCATH_SW		0
+#define REGW_HVCATH_CTRL	2
+
+//REGW_HVCATH_CTRL
+#define BIT_TRANSMIT		0
+#define BIT_ADCV_RESET		1
+
+
+#endif /* SRC_HV_CATHODE_H_ */
